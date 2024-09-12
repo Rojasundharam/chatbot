05f1abc3ba9df5c355186ed7e5efd84a988cc461
NisanthV <nisanth.v@jkkn.ac.in>
2024-09-12 11:36:18 +0530

    Fix import issues and improve functionality

--- embedding_utils.py
+++ embedding_utils.py
@@ -8,12 +8,14 @@ class EmbeddingUtil:
         self.model = SentenceTransformer(EMBEDDING_MODEL)
         self.index = None
         self.doc_ids = []
+        self.embeddings = None
 
     def create_embeddings(self, texts):
         return self.model.encode(texts)
 
     def create_faiss_index(self, embeddings, doc_ids):
         self.doc_ids = doc_ids
+        self.embeddings = embeddings
         dimension = embeddings.shape[1]
         self.index = faiss.IndexFlatIP(dimension)
         self.index.add(embeddings.astype('float32'))
@@ -21,4 +23,10 @@ class EmbeddingUtil:
     def search_similar(self, query, k=5):
         query_vector = self.create_embeddings([query])[0]
         D, I = self.index.search(np.array([query_vector]).astype('float32'), k)
-        return [self.doc_ids[i] for i in I[0]]
+        return [self.doc_ids[i] for i in I[0]], D[0]
+
+    def update_index(self, new_texts, new_doc_ids):
+        new_embeddings = self.create_embeddings(new_texts)
+        self.embeddings = np.vstack((self.embeddings, new_embeddings)) if self.embeddings is not None else new_embeddings
+        self.doc_ids.extend(new_doc_ids)
+        self.index.add(new_embeddings.astype('float32'))
